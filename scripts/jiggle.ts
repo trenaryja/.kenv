3b859b3ac869f5f90a6d93a816205ca8b1908375
Justin Trenary <justin.trenary@keylogic.com>
2024-05-10 09:19:54 -0400

    change jiggle to keep-awake

--- scripts/jiggle.ts
+++
@@ -1,6 +0,0 @@
-// Schedule: */2 9-17 * * *
-
-import '@johnlindquist/kit'
-
-await mouse.move([{ x: 1, y: 0 }])
-await mouse.move([{ x: -1, y: 0 }])
---
+++ scripts/keep-awake.ts
@@ -0,0 +1,5 @@
+// Schedule: */2 9-17 * * *
+
+import '@johnlindquist/kit'
+
+await keyboard.pressKey(Key.F24)
